5b667f8c438eb114523cc3529294d2442cfaaf5a
zerolake <zerolake@tuta.io>
2021-04-15 14:47:34 -0400

    added detail fields for PersonalDetails

--- src/Components/PersonalDetails.js
+++ src/Components/PersonalDetails.js
@@ -5,7 +5,17 @@ class PersonalDetails extends React.Component {
   render () {
     return (
       <div className='personal-details-container'>
-        <EditableText default='test' tag='h1' />
+        <EditableText default='First Name' tag='h1' />
+        <EditableText default='Last Name' tag='h1' />
+        <EditableText default='title/occupation' tag='h2'/>
+        <ul className='contact-details'>
+          <EditableText default='address 1' tag='li'/>
+          <EditableText default='city/state' tag='li'/>
+          <EditableText default='country' tag='li'/>
+          <EditableText default='post code' tag='li'/>
+          <EditableText default='phone' tag='li'/>
+          <EditableText default='email' tag='li'/>
+        </ul>
       </div>
     )
   }
